0ac276880a0cfa27ca8dd039089526d79c089bfe
Brendan Batliner <brendan.batliner@gmail.com>
2017-04-28 18:29:29 -0500

    SoundCloud OAuth in Firebase Cloud Functions

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,12 @@
 'use strict'
 
-// Imports
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')({ origin: true })
+const crypto = require('crypto')
 
 // Setup firebase-admin
 const serviceAccount = require('./service-account.json')
@@ -12,33 +15,104 @@ admin.initializeApp({
   databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`
 })
 
-// TODO: Should be a callback from Soundcloud OAuth, not an exposed route.
-exports.exchange = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const uid = req.query.uid
-    console.log('Got uid', uid)
-    admin.auth().createCustomToken(uid)
-      .then(customToken => {
-        res.send(customToken)
+function getClientId () {
+  return functions.config().soundcloud.client_id
+}
+
+function getClientSecret () {
+  return functions.config().soundcloud.client_secret
+}
+
+function getRedirectUri () {
+  return `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/popup.html`
+}
+
+function getConnectUrl (state) {
+  return 'https://soundcloud.com/connect?'
+    + `state=${state}`
+    + `&client_id=${encodeURIComponent(getClientId())}`
+    + `&redirect_uri=${encodeURIComponent(getRedirectUri())}`
+    + `&display=popup`
+    + `&response_type=code`
+    + `&scope=non-expiring`
+}
+
+function getOauthTokenFormData (code) {
+  const form = new FormData()
+  form.append('client_id', getClientId())
+  form.append('client_secret', getClientSecret())
+  form.append('redirect_uri', getRedirectUri())
+  form.append('grant_type', 'authorization_code')
+  form.append('code', code)
+  return form
+}
+
+exports.redirect = functions.https.onRequest((req, res) => {
+  cookieParser()(req, res, () => {
+    const state = req.cookies.state || crypto.randomBytes(20).toString('hex')
+    res.cookie('state', state.toString(), { maxAge: 300000, secure: true, httpOnly: true })
+    res.redirect(301, getConnectUrl(state.toString()))
+  })
+})
+
+exports.token = functions.https.onRequest((req, res) => {
+  cookieParser()(req, res, () => {
+    if (!req.cookies.state) {
+      res.send(401, 'State cookie not set or expired. Please try again.')
+      return
+    }
+    if (req.cookies.state !== req.query.state) {
+      res.send(401, 'State did not match. Please try again.')
+      return
+    }
+    const options = {
+      method: 'post',
+      body: getOauthTokenFormData(req.query.code)
+    }
+    // POST the authorization code for an OAuth token
+    fetch('https://api.soundcloud.com/oauth2/token', options).then(response => response.json())
+      // Use the token to fetch the user's profile
+      .then(data => {
+        const accessToken = data.access_token
+        const profilePromise = fetch(`https://api.soundcloud.com/me?oauth_token=${accessToken}`).then(response => response.json())
+        return Promise.all([accessToken, profilePromise])
+      })
+      // Create a Firebase user for the user
+      .then(([accessToken, profile]) => {
+        return createFirebaseAccount(accessToken, profile.id, profile.username, profile.avatar_url)
+      })
+      // Return the Firebase token to the JSONP callback
+      .then(firebaseToken => {
+        res.jsonp({ token: firebaseToken })
       })
       .catch(err => {
-        console.error('Error creating custom token:', err)
-        res.status(500).end()
+        console.error(err)
+        res.jsonp({ error: err.toString() })
       })
   })
 })
 
-// Doesn't work because external network request on free plan...
-// const request = require('superagent');
-// exports.helloWorld = functions.https.onRequest((req, res) => {
-//   const userId = 'djb00ts';
-//   request
-//     .get(`https://api.soundcloud.com/resolve.json?url=https://soundcloud.com/${userId}&client_id=z8LRYFPM4UK5MMLaBe9vixfph5kqNA25`)
-//     .end((err, response) => {
-//         if (err) {
-//             res.send(err);
-//         } else {
-//             res.send(response);
-//         }
-//     });
-// })
+function createFirebaseAccount(accessToken, soundcloudId, displayName, photoURL) {
+  // The new user's Firebase profile
+  const uid = soundcloudId.toString()
+  const profile = {
+    displayName,
+    photoURL
+  }
+
+  // Save the access token
+  const databasePromise = admin.database().ref(`/accessTokens/${uid}`).set(accessToken)
+
+  // Create or update the user account
+  const userCreationPromise = admin.auth().updateUser(uid, profile)
+    .catch(err => {
+      if (err.code === 'auth/user-not-found') {
+        return admin.auth().createUser(Object.assign({}, profile, { uid }))
+      }
+      throw err
+    })
+
+  return Promise.all([databasePromise, userCreationPromise]).then(() => {
+    return admin.auth().createCustomToken(uid)
+  })
+}
